4e204fd7a81148c37b193b7b7f4551d0a689b4a0
SeiteAlexMH <30759170+SeiteAlexMH@users.noreply.github.com>
2017-09-20 18:54:43 -0500

    Update Prime100.java

--- Prime100.java
+++ Prime100.java
@@ -1,22 +1,15 @@
-class Prime
-{
-	public static void main(String args[])
-	{
-		for (int i = 2; i < 100; i++)
-		{
-			int number = i;
-			boolean found = false;			
-			for (int j = 2; j < number; j++)
-			{
-				if (i%j == 0)
-				{
-					found = true;
-				}
-			}
-			if (!found)
-			{
-				System.out.println(number);
-			}
-		}
+class Prime{
+  public static void main(String args[]){
+    for (int i = 2; i < 100; i++){
+      boolean found = false;			
+      for (int j = 2; j < i; j++){
+	if (i%j == 0){
+	  found = true;
 	}
+      }
+      if (!found){
+	System.out.println(i);
+      }
+    }
+  }
 }
